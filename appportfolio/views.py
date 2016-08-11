197b6b5768f5ebad17b93d15a0c4c551cf6a943e
KiaraRaynS <sherman.rayn@outlook.com>
2016-08-11 14:17:29 -0400

    Basic template up

---
+++ appportfolio/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'indexview.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['github'] = 'https://github.com/KiaraRaynS'
+        context['linkedin'] = 'https://www.linkedin.com/in/kiara-sherman-b83564125'
+        return context
---
+++ portfolio_site/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+from appportfolio.views import IndexView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', IndexView.as_view(), name='indexview'),
+]
