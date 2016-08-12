20158c48b08ddabfd475afd5433348223353063d
KiaraRaynS <sherman.rayn@outlook.com>
2016-08-12 12:24:18 -0400

    Added buttons to index page

--- appportfolio/views.py
+++ appportfolio/views.py
@@ -14,3 +14,10 @@ class IndexView(TemplateView):
 
 class PortfolioView(TemplateView):
     template_name = 'portfolioview.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['link1'] = 'https://koquibilis.herokuapp.com/'
+        context['code1'] = 'https://github.com/KiaraRaynS/koquibilis'
+        context['code2'] = 'https://github.com/KiaraRaynS/urlshortner-06-16-16'
+        return context
